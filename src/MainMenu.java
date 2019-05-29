e92c6b9b9dffc8ee6561634b08e01dcd4f0b4cc8
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-05-29 08:35:18 +0800

    Edit: better formatting and general code.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,56 +1,46 @@
 import BrainGame.GUI.MainApplication;
-import java.awt.event.*;
+
 import javax.swing.*;
 
 
 public class MainMenu {
 
-    JFrame Menu = new JFrame("Brain Game");
-    JButton Start = new JButton("Play");
-    JButton Exit = new JButton("Exit");
-    ImageIcon picture = new ImageIcon("Brain.png");
-    JLabel imageLabel = new JLabel(picture);
-    int menuWidth = 100; //Width of each button/item on display
-    int menuHeight = 30;//Height of each button/item on display
-    int menuY = 460; //Button/item location on display
-    int WIDTH = 490; 
-    int HEIGHT = 530; 
+    private static final int menuWidth = 100, //Width of each button/item on display
+            menuHeight = 30, //Height of each button/item on display
+            menuY = 460, //Button/item location on display
+            WIDTH = 490,
+            HEIGHT = 530;
+    private JFrame Menu;
 
     public MainMenu() {
-        
+
+        Menu = new JFrame("Brain Game");
         Menu.setResizable(false);
         Menu.setSize(WIDTH, HEIGHT);
         Menu.setLayout(null);
         Menu.setLocationRelativeTo(null);
         Menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Start.setSize(menuWidth, menuHeight);
-        Start.setLocation(10, menuY);
-        Menu.add(Start);
-        Start.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                //here the action to move to next window
-                Menu.setVisible(false);
-                new MainApplication().run();
-                Menu.dispose();
-            }
-
+        JButton start = new JButton("Play");
+        start.setSize(menuWidth, menuHeight);
+        start.setLocation(10, menuY);
+        Menu.add(start);
+        start.addActionListener(e -> {
+            //here the action to move to next window
+            Menu.setVisible(false);
+            new MainApplication().run();
+            Menu.dispose();
         });
-        
-        
-        Exit.setSize(menuWidth, menuHeight);
-        Exit.setLocation(375, menuY);
-        Menu.add(Exit);
-        Exit.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        
+
+        JButton exit = new JButton("Exit");
+        exit.setSize(menuWidth, menuHeight);
+        exit.setLocation(375, menuY);
+        Menu.add(exit);
+        exit.addActionListener(e -> System.exit(0));
+
+        ImageIcon picture = new ImageIcon("Brain.png");
+        JLabel imageLabel = new JLabel(picture);
         imageLabel.setBounds(0, 0, 500, 500);
         imageLabel.setVisible(true);
         Menu.add(imageLabel);
